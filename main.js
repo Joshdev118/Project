0ef60cf7f178ee5c7a25eaad931d84264dbdc492
Sim Joshua <simj12705@gmail.com>
2023-12-03 18:28:00 +0800

    Day 4: still working on img

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const img1 = document.getElementById("seb1");
+const img2 = document.getElementById("seb2");
+const img3 = document.getElementById("seb3");
+
+let slide = 0;
+const maxSlide = 2;
+
+img1.style.display = "flex";
+img2.style.display = "none";
+img3.style.display = "none";
+
+
+
 
 
 
