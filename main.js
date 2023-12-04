2cb7bdfdd75c7989b6a364b70a9148d52dd70c70
Sim Joshua <simj12705@gmail.com>
2023-12-04 21:25:08 +0800

    done img gallery

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ const img3 = document.getElementById("seb3");
 let slide = 0;
 const maxSlide = 2;
 
-img1.style.display = "flex";
-img2.style.display = "none";
-img3.style.display = "none";
-
+function nextSlide(num) {
+    const slides = document.getElementsByClassName("img-seb");
+    slides[slide].style.display = "none";
+    slide = slide + num;
+    if (slide > 2) {
+        slide = 0;
+    }else if (slide < 0) {
+        slide = 2;
+    }
+    slides[slide].style.display = "block";
+}
 
 
 
